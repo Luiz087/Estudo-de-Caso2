3f48767c275bb87e3025d53f71ea6f9b05984c8a
Luiz <luiz.s08@aluno.ifsc.edu.br>
2023-05-08 10:00:29 -0300

    Tela ADMN

--- src/visao/TelaAdminPrincipal.java
+++ src/visao/TelaAdminPrincipal.java
@@ -6,6 +6,10 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLabel;
+import java.awt.ScrollPane;
 
 public class TelaAdminPrincipal extends JFrame {
 
@@ -32,17 +36,46 @@ public class TelaAdminPrincipal extends JFrame {
 	 */
 	public TelaAdminPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1920, 1080);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(0, 0, 255));
-		panel.setBounds(0, 0, 434, 261);
-		contentPane.add(panel);
+		JButton btnNewButton_1_1_1 = new JButton("Adicionar Funcionário");
+		btnNewButton_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton_1_1_1.setBounds(230, 55, 233, 71);
+		contentPane.add(btnNewButton_1_1_1);
+		
+		JLabel lblNewLabel = new JLabel("Funções de Administrador");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(241, 11, 217, 33);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblFunesDeFuncionrio = new JLabel("Funções de Funcionário");
+		lblFunesDeFuncionrio.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblFunesDeFuncionrio.setBounds(601, 11, 217, 33);
+		contentPane.add(lblFunesDeFuncionrio);
+		
+		JButton btnNewButton_1_1_1_1 = new JButton("Adicionar Funcionário");
+		btnNewButton_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton_1_1_1_1.setBounds(590, 55, 233, 71);
+		contentPane.add(btnNewButton_1_1_1_1);
+		
+		JButton btnNewButton_1_1_1_2 = new JButton("Remover Funcionário");
+		btnNewButton_1_1_1_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton_1_1_1_2.setBounds(230, 156, 233, 71);
+		contentPane.add(btnNewButton_1_1_1_2);
+		
+		JButton btnNewButton_1_1_1_2_1 = new JButton("Alterar Funcionário");
+		btnNewButton_1_1_1_2_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton_1_1_1_2_1.setBounds(230, 264, 233, 71);
+		contentPane.add(btnNewButton_1_1_1_2_1);
+		
+		JButton btnNewButton_1_1_1_2_1_1 = new JButton("Listar Funcionários");
+		btnNewButton_1_1_1_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton_1_1_1_2_1_1.setBounds(230, 375, 233, 71);
+		contentPane.add(btnNewButton_1_1_1_2_1_1);
 	}
-
 }
